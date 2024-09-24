34419a5a4be1847461ff3827fed105f58cbcee46
jangkeesung <77503527+jangkeesung@users.noreply.github.com>
2024-09-24 23:49:08 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.91 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,24 +1,25 @@
 import java.util.*;
-class Solution {
+
+class Solution {// 성능비교테스트
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> list = new LinkedList<>();
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<(k>score.length ? score.length : k); i++) {
-            list.add(score[i]);
-            min = Math.min(score[i], min);
-            answer[i] = min;
-        }
-        list.sort(Comparator.reverseOrder());
-        for(int i=k; i<score.length; i++) {
-            for(int j=0; j<k; j++) {
-                if(score[i] > list.get(j)) {
-                    list.add(j, score[i]);
-                    break;
-                }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        int temp = 0;
+
+        for(int i = 0; i < score.length; i++) {
+
+            priorityQueue.add(score[i]);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
             }
-            answer[i] = list.get(k-1);
+
+            answer[i] = priorityQueue.peek();
         }
+
+
+
         return answer;
     }
 }
