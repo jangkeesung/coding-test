66f44047e4d05aff8358a490321a137780e205e8
jangkeesung <77503527+jangkeesung@users.noreply.github.com>
2024-10-01 23:06:21 +0900

    [level 1] Title: 소수 찾기, Time: 218.52 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for(int i=2; i<=n; i++) {
-            for(int j=2; j*j<=i; j++) {
+        int answer = 1;
+        for(int i=3; i<=n; i+=2) {
+            for(int j=3; j*j<=i; j++) {
                 if(i%j==0) {answer--; break;}
             }
             answer++;
